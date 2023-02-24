b0ec1b626ea8c174076c7d7a5d0a4fc98ea8a191
AnriB <anribardhi25@gmail.com>
2023-02-24 18:27:06 +0100

    minor changes

--- instances.tf
+++ instances.tf
@@ -28,9 +28,9 @@ resource "aws_instance" "control-plane" {
   provisioner "remote-exec" {
     inline = [
       "sudo sleep 120",
-      "sudo chmod 644 /etc/hosts",
-      "sudo echo '${self.private_ip} ${aws_instance.control-plane.tags.Name}' >> /etc/hosts",
-      "sudo hostnamectl set-hostname ${aws_instance.control-plane.tags.Name}"
+      "sudo su",
+      "echo '${self.private_ip} ${aws_instance.control-plane.tags.Name}' >> /etc/hosts",
+      "hostnamectl set-hostname ${aws_instance.control-plane.tags.Name}"
     ]
     connection {
       type = "ssh"
@@ -69,9 +69,9 @@ resource "aws_instance" "workers" {
   provisioner "remote-exec" {
     inline = [
       "sudo sleep 120",
-      "sudo chmod 644 /etc/hosts",
-      "sudo echo '${self.private_ip} ${aws_instance.workers[count.index + 1].tags.Name}' >> /etc/hosts",
-      "sudo hostnamectl set-hostname ${aws_instance.workers[count.index + 1].tags.Name}"
+      "sudo su",
+      "echo '${self.private_ip} ${aws_instance.workers[count.index + 1].tags.Name}' >> /etc/hosts",
+      "hostnamectl set-hostname ${aws_instance.workers[count.index + 1].tags.Name}"
     ]
     connection {
       type = "ssh"
